c4d8633acebc82d69d11b1afc04b29f93a5bf649
John <johnerik63@gmail.com>
2022-06-03 22:25:29 -0300

    O que aprendi nas aulas de Programação de soluções computacionais ate agora.

---
+++ ExemploFor/src/ExemploFor.java
@@ -0,0 +1,24 @@
+package uam;
+
+public class ExemploFor {
+
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        while (contador < 10) {
+
+            System.out.println("Hello World");
+
+            contador++;
+            }
+
+        System.out.println("********************");
+
+        for(int cont = 0; cont < 10; cont++){
+
+            System.out.println("Hello com for");
+
+        }
+    }
+}
