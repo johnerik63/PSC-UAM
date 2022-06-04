e2aea2f856b992d18597eded906f39d3a8c3e9d4
John <johnerik63@gmail.com>
2022-06-03 22:27:37 -0300

    O que aprendi nas aulas de Programação de soluções computacionais ate agora.

---
+++ ExemploRepeticao/src/ExemploRepeticao.java
@@ -0,0 +1,21 @@
+package uam;
+
+import java.util.Scanner;
+
+public class ExemploRepeticao {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+
+        System.out.println("digite a sua nota: ");
+        n1 = entrada.nextInt();
+
+        System.out.println("digite a sua nota: ");
+        n2 = entrada.nextInt();
+
+
+    }
+}
