6fdf2df860214161a600d98ba7fc3e87f9ce7e65
John <johnerik63@gmail.com>
2022-06-09 21:16:34 -0300

    O que aprendi nas aulas

---
+++ desafio6/src/desafio6.java
@@ -0,0 +1,15 @@
+package uam;
+
+public class desafio6 {
+
+    public static void main(String[] args) {
+
+        mensagem();
+        mensagem();
+        mensagem();
+
+    }
+    public static void mensagem() {
+        System.out.println("Meu primeiro programa com funcoes");
+    }
+}
